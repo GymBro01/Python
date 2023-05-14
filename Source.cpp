5f01b35650e3a68f09a8dbe66f4ec5aa9b95c274
GymBro01 <133540079+GymBro01@users.noreply.github.com>
2023-05-14 19:40:26 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,12 @@
-﻿#include<iostream>
-#include<string>
+﻿#include<string>
 #include<math.h>
 #include<algorithm>
 #include <vector> 
 #include<conio.h>
 #include<Windows.h>
+#include<iostream>
 using namespace std;
 
-#define APPLE(a) a.pos.x = rand() * rand() % 67 + 26;a.pos.y = rand() * rand() % 28 + 2;if (a.pos.x % 2 == 1){	a.pos.x += 1;}TO_POSITION(a.pos.y, a.pos.x);for(int i=0;i<s.l;i++){if(a.pos.x==s.body[i].x&& a.pos.y == s.body[i].y){ a.pos.x = rand() * rand() % 67 + 26;a.pos.y = rand() * rand() % 28 + 2;if (a.pos.x % 2 == 1){	a.pos.x += 1;}TO_POSITION(a.pos.y, a.pos.x);i=0;}}cout << a.apple; 
-#define FIELD cout << WHITE "\t\t\t";for (int i = 0; i < 70;i++){cout << wall;} cout<< YELLOW <<"\tPOINTS: "<<WHITE; for (int i = 0; i < 30; i++){cout << "\n\t\t\t"<<wall;for (int i = 0; i < 68; i++){cout << ' ';}cout << wall;}cout << "\n\t\t\t";for (int i = 0; i < 70; i++){cout << wall;}
 #define RED "\033[31;1m" 
 #define GREEN "\033[32m" 
 #define WHITE "\033[37m" 
@@ -34,9 +32,9 @@ using namespace std;
 class Pos
 {
 public:
-	int x=70, y=17;
+	int x = 70, y = 17;
 	Pos() {}
-	Pos(int x,int y)
+	Pos(int x, int y)
 	{
 		this->x = x;
 		this->y = y;
@@ -52,35 +50,6 @@ public:
 			return false;
 	}
 };
-class Snake
-{
-public:
-	int p=0;
-	Pos* body = new Pos[3];
-	char turn='d';
-	int l=3;
-	string snake = "O";
-	string snakeH = "@";
-	Snake(){}
-
-	void add()
-	{
-		Pos* copy = body;
-		body = new Pos[l + 1];
-		for (int i = 0; i < l; i++)
-		{
-			body[i] = copy[i];
-		}
-	}
-};
-
-class Apple
-{
-public:
-	Apple() {}
-	Pos pos;
-	string apple = RED "*" DEFAULT_COLOR;
-};
 
 
 int main()
@@ -89,133 +58,288 @@ int main()
 	cursor.bVisible = false;
 	cursor.dwSize = 1;
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
-
-	Apple a;
-	Apple a1;
-	Apple a2;
-	char wall='#';
-	FIELD;
-	bool go=false;
 	
-	Snake s;
-	char ch=' ';
-	
-	APPLE(a);
-	APPLE(a1);
-	APPLE(a2);
 
+
+bool turnX;
+
+
+for (int i = 0; i < 4; i++)
+{
+	cout << "\n\t\t\t\t\t\t##      ##";
+}
+
+	cout << "\n\t\t\t\t          ";
+	for (int r = 0; r < 22; r++)
+	{
+		cout << "#";
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		cout << "\n\t\t\t\t\t\t##      ##";
+	}
+	cout << "\n\t\t\t\t          ";
+	for (int r = 0; r < 22; r++)
+	{
+		cout << "#";
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		cout << "\n\t\t\t\t\t\t##      ##";
+	}
+	Pos field [3][3];
+	
+	field[0][0] = { 43,2 };
+	int y = 0; int x = 0;
+	for (int i = 0; i < 3; i++,y+=5,x=0)
+	{
+		for (int r = 0; r < 3; r++, x+=8)
+		{
+			field[i][r].x = field[0][0].x + x;
+			field[i][r].y = field[0][0].y + y;
+		}
+	}
+	char ch;
 	while (true)
 	{
-		SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
-		TO_POSITION(1, 104);
-		cout<< CYAN << s.p<<DEFAULT_COLOR;
-		TO_POSITION(s.body[1].y, s.body[1].x);
-		cout << s.snake;
-		TO_POSITION(s.body[0].y, s.body[0].x);
-		cout << YELLOW <<s.snakeH<< DEFAULT_COLOR;
-		Sleep(100);
-		if (_kbhit())
-		{		
-			ch = char(_getch());		
-			if (ch == 'a' || ch == 's' || ch == 'd' || ch == 'w')
+		Pos cur=field[1][1];
+		while (ch != ' ')
+		{
+			cin >> ch;
+			switch (ch)
 			{
-							
-				if(abs(s.turn-ch)!=3&&abs(s.turn-ch)!=4)
-				s.turn = ch;
+			case 'a':
+			{
+				if (cur.x != field[0][0].x)
+				{
+					cur.x = field[0][0].x;
+				}
+				break;
 			}
-		}
-		TO_POSITION(s.body[s.l - 1].y, s.body[s.l - 1].x);
-		cout << ' ';
-		for (int i = s.l-1; i > 0 ; i--)
-		{
-			s.body[i] = s.body[i - 1];
-		}
-
-		switch (s.turn)
-		{
-		case 'w': 
-		{
-			s.body[0].y--;
-			break;
-		}
-		case 's':
-		{
-			s.body[0].y++;
-			break;
-		}
-		case 'a':
-		{
-			s.body[0].x-=2;
-			break;
-		}
-		case 'd':
-		{
-			s.body[0].x+=2;
-			break;
-		}
-		default:
-			break;
-		}
-      	if (s.body[0] == a.pos)
-		{
-			s.add();
-			s.l += 1;
-			s.p++;
-			APPLE(a)
-		}
-		if (s.body[0] == a1.pos)
-		{
-			s.add();
-			s.l += 1;
-			s.p++;
-			APPLE(a1)
-		}
-		if (s.body[0] == a2.pos)
-		{
-			s.add();
-			s.l += 1;
-			s.p++;
-			APPLE(a2)
-		}
-
-		for (int i = 1; i < s.l; i++)
-		{
-			if (s.body[0] == s.body[i])
+			case 'w':
 			{
-				go = true;
+				if (cur.y != field[0][0].y)
+				{
+					cur.y = field[0][0].y;
+				}
+				break;
+			}
+			case 's':
+			{
+				if (cur.y != field[2][0].y)
+				{
+					cur.y = field[2][0].y;
+				}
+				break;
+			}
+			case 'd':
+			{
+				if (cur.x != field[0][2].x)
+				{
+					cur.x = field[0][2].x;
+				}
+				break;
+			}
+			default:
 				break;
 			}
 		}
-		if(s.body[0].x==94|| s.body[0].x == 25)
-		{
-			go = true;
-		
-		}
-		if (s.body[0].y == 1 || s.body[0].y == 32)
-		{
-			go = true;
-			
-		}
+	}
+}
 
-		if (go)
-		{
-			TO_POSITION(s.body[0].y, s.body[0].x);
-			cout << RED << 'X';
-			Sleep(300);
-			for (int i = 1; i < s.l; i++)
-			{
-				TO_POSITION(s.body[i].y, s.body[i].x);
-				cout << 'X';
-				Sleep(300);
-			}
-			system("cls");
-			cout << "\t\t\t\t #######       #####      ##       ##    #########\n\t\t\t\t##      #    ##     ##    ###     ###    ##\n\t\t\t\t##           ##     ##    ## ##### ##    ##\n\t\t\t\t##   ####    #########    ##  ###  ##    ########\n\t\t\t\t##     ##    ##     ##    ##       ##    ##\n\t\t\t\t #######     ##     ##    ##       ##    #########\n\n\n\t\t\t\t  #####     ##       ##    #########    #######\n\t\t\t\t##     ##    ##     ##     ##           ##    ##\n\t\t\t\t##     ##    ##     ##     ##           #######\n\t\t\t\t##     ##     ##   ##      ########     ####\n\t\t\t\t##     ##      ## ##       ##           ##  ##\n\t\t\t\t  #####         ###        #########    ##    ##";
-			break;
-		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class An
+{
+public:
+	string name="Petya", stat="";
+	An() {}
+
+	virtual void say()
+	{
+		cout << "I'm An";
+	}
+	void Name()
+	{
+		cout << name;
+	}
+	virtual void type()
+	{
+		cout << "I'm An";
+	}
+
+};
+
+class Dog:public An
+{
+public:
+
+	Dog() {}
+
+	virtual void say()
+	{
+		cout << "I'm Dog";
+	}
+	virtual void type()
+	{
+		cout << "I'm Dog";
+	}
+};
+
+class Cat :public An
+{
+public:
+
+	Cat() {}
+
+	 void say()
+	{
+		cout << "I'm Cat";
+	}
+	virtual void type()
+	{
+		cout << "I'm Cat";
+	}
+};
+
+class Hamster :public An
+{
+public:
+
+	Hamster() {}
+
+	virtual void say()
+	{
+		cout << "I'm Colobok";
+	}
+	virtual void type()
+	{
+		cout << "I'm Colobok";
+	}
+};
+
+
+
+int main()
+{
+	Dog d;
+	Cat c;
+	Hamster h;
+	An an;
+	An* a[4] ={&d,&c,&h,&an};
+
+	for (int i = 0; i < 4; i++)
+	{
+		a[i]->say();
+		cout << endl;
+		a[i]->type();
+		cout << endl;
+		a[i]->Name();
+		cout << endl;
 
 	}
-	
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+class list
+{
+public:
+	int* v = new int[0];
+	int l=0;
+	list() {}
+	void add(int x)
+	{
+		int* copy = v;
+		l += 1;
+		v = new int[l];
+		for (int i = 0; i < l - 1; i++)
+		{
+			v[i] = copy[i];
+		}
+		v[l - 1] = x;
+	}
+
+	virtual	void del()
+	{
+
+	}
+};
+
+class queue :public list
+{
+public:
+	queue() {}
+	virtual	void del()
+	{
+		int* copy = v;
+		l -= 1;
+		v = new int[l];
+		
+		for (int i = 0; i<l; i++)
+		{
+			v[i] = copy[i + 1];
+		}
+	}
+};
+
+class stack :public list
+{
+public:
 	
 
-}
+	virtual	void del()
+	{
+		int* copy = v;
+		l -= 1;
+		v = new int[l];
+		for (int i = 0; i < l; i++)
+		{
+			v[i] = copy[i];
+		}
+	}
+	stack() {}
+};
+
+
+int main()
+{
+	list* l[1];
+	queue s;
+	l[0] = &s;
+	l[0]->add(1);
+	l[0]->add(2);
+	cout << l[0]->v[0] << " " << l[0]->v[1]<<" ";
+	l[0]->del();
+	cout << l[0]->v[0] << " " << l[0]->v[1];
+
+}*/
+
+
 
